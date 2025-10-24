eda0a1d2351ad1cdf33483c425994c137d8a1850
OJPARKINSON <olliepar01@gmail.com>
2025-10-25 00:54:33 +0100

    dockerising application

--- telemetryService/golang/go.mod
+++ telemetryService/golang/go.mod
@@ -5,6 +5,7 @@ go 1.25.1
 require github.com/rabbitmq/amqp091-go v1.10.0
 
 require (
+	github.com/joho/godotenv v1.5.1
 	github.com/questdb/go-questdb-client v1.0.5 // indirect
 	github.com/questdb/go-questdb-client/v4 v4.0.0
 	google.golang.org/protobuf v1.36.10 // indirect
--- telemetryService/golang/main.go
+++ telemetryService/golang/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/ojparkinson/telemetryService/internal/config"
 	"github.com/ojparkinson/telemetryService/internal/persistance"
@@ -11,9 +13,13 @@ import (
 func main() {
 	fmt.Println("Starting subscriber")
 
+	questdbHost := getEnv("QUESTDB_HOST", "localhost")
+	questdbPort := getEnvInt("QUESTDB_PORT", 9000)
+	poolSize := getEnvInt("SENDER_POOL_SIZE", 10)
+
 	config := config.NewConfig()
 
-	senderPool, err := persistance.NewSenderPool(10, "localhost", 9000)
+	senderPool, err := persistance.NewSenderPool(poolSize, questdbHost, questdbPort)
 	if err != nil {
 		fmt.Println("Failed to create sender pool: ", err)
 	}
@@ -21,4 +27,20 @@ func main() {
 
 	fmt.Println("Starting to consume")
 	messaging.Subscribe(config)
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvInt(key string, defaultValue int) int {
+	if value := os.Getenv(key); value != "" {
+		if intVal, err := strconv.Atoi(value); err == nil {
+			return intVal
+		}
+	}
+	return defaultValue
 }
