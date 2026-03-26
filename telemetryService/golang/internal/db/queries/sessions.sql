359c3de84224c41469973109e050aa269f596495
OJPARKINSON <olliepar01@gmail.com>
2026-03-26 16:05:14 +0100

    update sessions query to get the max int not max from a string

--- telemetryService/golang/internal/db/queries/sessions.sql
+++ telemetryService/golang/internal/db/queries/sessions.sql
@@ -1,5 +1,5 @@
 -- name: ListSessions :many
-SELECT DISTINCT session_id, track_name, session_name, MAX(lap_id) as max_lap_id, MAX(timestamp) as last_updated 
+SELECT DISTINCT session_id, track_name, session_name, MAX(CAST(lap_id as int)) as max_lap_id, MAX(timestamp) as last_updated
 FROM TelemetryTicks
 WHERE session_name = 'RACE' AND lap_id > 0
 GROUP BY session_id, track_name, session_name
