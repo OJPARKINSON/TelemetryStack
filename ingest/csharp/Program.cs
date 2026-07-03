7718dc58345377b9e3f88171132f53917d36b2dd
OJPARKINSON <olliepar01@gmail.com>
2026-07-03 21:43:08 +0200

    Starting a basic cshap ingest

---
+++ ingest/csharp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.CommandLine;
+
+var rootCmd = new RootCommand("process");
+
+var telemetryDirPath = new Option<string>("--path", "-p")
+{
+    Description = "Path to the IRacing Telemetry folder"
+};
+
+Console.WriteLine("Welcome to telemetry stack ingest service!");
