82b22528c5be8e07861e1e79e184ee1eb288af9a
OJPARKINSON <olliepar01@gmail.com>
2026-07-07 16:54:44 +0200

    clean up old files

--- dashboard/components/telemetry/index.ts
+++
@@ -1,6 +0,0 @@
-export { GPSAnalysisPanel } from "./GPSAnalysisPanel";
-export { HoverMarker } from "./HoverMarker";
-export { MemoizedRacingLine } from "./RacingLine";
-export { TelemetryMapSection } from "./TelemetryMapSection";
-export { TelemetrySidebar } from "./TelemetrySidebar";
-export { TelemetryStatsHeader } from "./TelemetryStatsHeader";
